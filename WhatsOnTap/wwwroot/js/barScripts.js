2d1465272511ac46e97f8ddac6ed6a02aa414462
kevinahn7 <kevinahn7@hotmail.com>
2018-07-23 15:26:58 -0700

    Add styling for grids in beer index and details

--- WhatsOnTap/wwwroot/js/barScripts.js
+++ WhatsOnTap/wwwroot/js/barScripts.js
@@ -1,40 +1,40 @@
-$(document).ready(function() {
-
-  function initMap() {
-    var options = {
-      zoom: 8,
-      center: {lat: 45.524169, lng: -122.672927}
-    };
-
-    var map = new google.maps.Map(document.getElementById('map'), options);
-
-
-    function addMarker(props) {
-      var marker = new google.maps.Marker({
-        position: props.coords,
-        map: map;
-      });
-
-      var infoWindow = new google.maps.infoWindow({
-        content: "<a href='/bars/" + props.id + ">" + props.content + "</a>"
-      });
-
-    };
-
-
-    marker.addListener("click", function() {
-      infoWindow.open(map, marker);
-    });
-
-
-    addMarker({
-      coords: {lat: 45, lng: -122),
-      content: "Name",
-      id: 3
-    }});
-
-
-
-  };
-
-});
+// $(document).ready(function() {
+//
+//   function initMap() {
+//     var options = {
+//       zoom: 8,
+//       center: {lat: 45.524169, lng: -122.672927}
+//     };
+//
+//     var map = new google.maps.Map(document.getElementById('map'), options);
+//
+//
+//     function addMarker(props) {
+//       var marker = new google.maps.Marker({
+//         position: props.coords,
+//         map: map;
+//       });
+//
+//       var infoWindow = new google.maps.infoWindow({
+//         content: "<a href='/bars/" + props.id + ">" + props.content + "</a>"
+//       });
+//
+//     };
+//
+//
+//     marker.addListener("click", function() {
+//       infoWindow.open(map, marker);
+//     });
+//
+//
+//     addMarker({
+//       coords: {lat: 45, lng: -122),
+//       content: "Name",
+//       id: 3
+//     }});
+//
+//
+//
+//   };
+//
+// });
