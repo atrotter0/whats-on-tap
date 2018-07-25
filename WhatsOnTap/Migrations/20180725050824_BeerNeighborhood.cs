0e2b552bfb2be4392bd3c76c48adcc800f55015c
Elly Maimon <eliranmaimon@gmail.com>
2018-07-25 09:48:40 -0700

    Add BarNeighborhood property and run new migration

---
+++ WhatsOnTap/Migrations/20180725050824_BeerNeighborhood.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WhatsOnTap.Migrations
+{
+    public partial class BeerNeighborhood : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BarNeighborhood",
+                table: "Bars",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BarNeighborhood",
+                table: "Bars");
+        }
+    }
+}
