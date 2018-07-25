06d85b7cb4a171ad69f6b5375d4b747f78e66964
Elly Maimon <eliranmaimon@gmail.com>
2018-07-25 09:49:00 -0700

    Add filtering to bars by neighborhood and by minimum rating

--- WhatsOnTap/Controllers/BarsController.cs
+++ WhatsOnTap/Controllers/BarsController.cs
@@ -5,6 +5,7 @@ using WhatsOnTap.Models;
 using WhatsOnTap.ViewModels;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 
 namespace WhatsOnTap.Controllers
 {
@@ -26,6 +27,36 @@ namespace WhatsOnTap.Controllers
             BarDetailsViewModel viewModel = new BarDetailsViewModel(_db, id);
             viewModel.FindBarBeers(id);
             return View(viewModel);
+        }
+
+        [HttpGet("bars/filter")]
+        public ActionResult FilterBy(string barNeighborhood, int barRating)
+        {
+            if (barNeighborhood == null && barRating == 0)
+            {
+                var matches = _db.Bars.ToList();
+                return View ("Index", matches);
+            }
+            else if (barRating == 0)
+            {
+                var matches = _db.Bars.Where(entry => entry.BarNeighborhood == barNeighborhood);
+                return View ("Index", matches);
+            }
+            else if (barNeighborhood == null)
+            {
+                var matches = _db.Bars.Where(entry => entry.BarRating >= barRating)
+                                      .OrderByDescending(entry => entry.BarRating)
+                                      .ToList();
+                return View ("Index", matches);
+            }
+            else
+            {
+                var matches = _db.Bars.Where(entry => entry.BarNeighborhood == barNeighborhood)
+                                      .Where(entry => entry.BarRating >= barRating)
+                                      .OrderByDescending(entry => entry.BarRating)
+                                      .ToList();
+                return View ("Index", matches);
+            } 
         }
     }
 }
