e63882a9208bca187a542a2bff7e80cd2491cb5d
Kevin Ahn <kevinahn7@hotmail.com>
2018-07-25 10:58:48 -0700

    Add Create method for bars

--- WhatsOnTap/Controllers/BarsController.cs
+++ WhatsOnTap/Controllers/BarsController.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System;
 using Microsoft.AspNetCore.Mvc;
 using WhatsOnTap.Models;
 using WhatsOnTap.ViewModels;
@@ -26,6 +27,35 @@ namespace WhatsOnTap.Controllers
             BarDetailsViewModel viewModel = new BarDetailsViewModel(_db, id);
             viewModel.FindBarBeers(id);
             return View(viewModel);
+        }
+
+        [HttpGet("/bars/new")]
+        public ActionResult Create() => View(_db.Beers.ToList());
+
+        [HttpPost("/bars/new")]
+        public ActionResult Create(List<int> BeerId, string barName, string barRating, string barWebsite, string barStreet, string barCity, string barState, string barZip, string barPhone, string barLatitude, string barLongitude)
+        {
+            Bar newBar = new Bar();
+            newBar.BarName = barName;
+            newBar.BarRating = int.Parse(barRating);
+            newBar.BarWebsite = barWebsite;
+            newBar.BarStreet = barStreet;
+            newBar.BarCity = barCity;
+            newBar.BarState = barState;
+            newBar.BarZip = barZip;
+            newBar.BarPhone = barPhone;
+            newBar.BarLatitude = Convert.ToDouble(barLatitude);
+            newBar.BarLongitude = Convert.ToDouble(barLongitude);
+
+            _db.Add(newBar);
+            foreach (int beerId in BeerId)
+            {
+                Taplist newTaplist = new Taplist(beerId, newBar.BarId);
+                _db.Taplists.Add(newTaplist);
+            }
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
