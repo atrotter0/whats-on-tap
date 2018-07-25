c9947c136ea01be37cf6f8d7bcb7f2ff17ad257b
Abel Trotter <a_trotter0@hotmail.com>
2018-07-25 07:51:56 -0700

    Add data annototations to ProfileViewModel

--- WhatsOnTap/ViewModels/ProfileViewModel.cs
+++ WhatsOnTap/ViewModels/ProfileViewModel.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WhatsOnTap.ViewModels
 {
     public class ProfileViewModel
     {
+        [EmailAddress]
         public string Email { get; set; }
+
+        [DataType(DataType.Password)]
         public string CurrentPassword { get; set; }
+
+        [DataType(DataType.Password)]
         public string NewPassword { get; set; }
 
         public ProfileViewModel() {}
