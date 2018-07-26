0769b5f0d8ba4663c0c98589192e8daa1be44fd3
Abel Trotter <a_trotter0@hotmail.com>
2018-07-25 22:49:16 -0700

    Add back-end validation for register user

--- WhatsOnTap/ViewModels/RegisterViewModel.cs
+++ WhatsOnTap/ViewModels/RegisterViewModel.cs
@@ -14,12 +14,14 @@ namespace WhatsOnTap.ViewModels
         [Display(Name = "Password")]
         public string Password { get; set; }
 
+        [Required]
         [DataType(DataType.Password)]
         [Display(Name = "Confirm Password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
 
         [Required]
+        [Display(Name = "Account Type")]
         public string UserType { get; set; }
     }
 }
